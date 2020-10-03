83964fef0c06054a23f771c86324755bfce318d3
ishaksengul <sengulishak@gmail.com>
2020-10-03 16:29:06 +0200

    commit 4. satir

--- src/Ders01.java
+++ src/Ders01.java
@@ -5,6 +5,7 @@ public class Ders01 {
 		System.out.println("Saat 14:53");
 		System.out.println("Saat 16:03");
 		System.out.println("Saat 16:24");
+		System.out.println("Saat 16:28 - ishak");
 		
 	}
 	
