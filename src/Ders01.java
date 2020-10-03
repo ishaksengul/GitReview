2367c8d289b3cf18f0b3d9dfa4ca01beeb5e421a
ishaksengul <67099229+ishaksengul@users.noreply.github.com>
2020-10-03 16:25:01 +0200

    Update Ders01.java
    commit changes

--- src/Ders01.java
+++ src/Ders01.java
@@ -4,6 +4,7 @@ public class Ders01 {
 	public static void main(String[] args) {
 		System.out.println("Saat 14:53");
 		System.out.println("Saat 16:03");
+		System.out.println("Saat 16:24");
 		
 	}
 	
