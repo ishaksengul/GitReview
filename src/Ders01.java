e462a9adc86213328e1d7379a7a4aec44c87c5d5
ishaksengul <67099229+ishaksengul@users.noreply.github.com>
2020-10-04 14:19:59 +0200

    Update Ders01.java

--- src/Ders01.java
+++ src/Ders01.java
@@ -8,10 +8,7 @@ public class Ders01 {
 		System.out.println("Saat 16:30 - ishak conflict");
 		System.out.println("Saat 13:48 feature brach 1. satir");
 		System.out.println("Saat 13:54 feature branch 2. satir");
-<<<<<<< HEAD
 		System.out.println("feature 3. satir");
-=======
->>>>>>> 207c2cd2b169d4cbbf9c63d2c1dcf5cd6297db78
 
 
 		
