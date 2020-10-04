9c4dd0fd1347a7b9b36c9bfdf934b47b92c35b28
ishaksengul <sengulishak@gmail.com>
2020-10-04 13:51:03 +0200

    feature branch'te commit

--- src/Ders01.java
+++ src/Ders01.java
@@ -6,6 +6,8 @@ public class Ders01 {
 		System.out.println("Saat 16:03");
 		System.out.println("Saat 16:24");
 		System.out.println("Saat 16:30 - ishak conflict");
+		System.out.println("Saat 13:48 feature brach 1. satir");
+		
 
 		
 	}
